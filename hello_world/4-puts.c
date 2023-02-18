c18bad16af9706d9e61012c366cb4536096be8b5
[TaniaHillock] <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@github.com>
2023-02-17 18:15:26 -0800

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,13 +2,10 @@
 /**
  *main - is described as a function
  *it prints a sentence
+ *return - this wil always return 0
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-/**
- *Return: will allways be a zero
- *return: 0 to be valid
-*/
 }
