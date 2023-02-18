9d1d3f91abae154f4dec57431e66ae6834a3e0ac
[TaniaHillock] <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@github.com>
2023-02-17 20:12:41 -0800

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-  printf("Size of a char: %c byte(s)\n", sizeof(char));
-  printf("Size of an int: %i bytes(s)\n", sizeof(int));
+  printf("Size of a char: %d byte(s)\n", sizeof(char));
+  printf("Size of an int: %d bytes(s)\n", sizeof(int));
   printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
-  printf("Size of a long long int: %f byte(s)\n" sizeof(float));
+  printf("Size of a long long int: %d byte(s)\n", sizeof(float));
 return (0);
 }
