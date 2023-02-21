106483cea94b24fd244a1ad1019215adbc2b6aee
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-21 01:08:00 -0800

    .0-positive_or_negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-* main - assigns a random number.
-* return - (O) to success.
-*/
+/* main - assigns a random number.*/
 int main(void)
 {
 	int n;
