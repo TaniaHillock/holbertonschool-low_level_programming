00566e0d29acbff118841568da9d5d967e379a5c
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-21 10:02:14 -0800

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,18 @@
-#inlcude <stdio.h>
+#include <stdio.h>
 /**
- * main - prints lowercase alphabet
+ * main - prints single digit numbers
+ * starting from 0
  * followed by a new line.
  * Return: (0) to success
 */
 int main(void)
 {
-  
-  return(0);
+int i;
+
+for(i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
 }
