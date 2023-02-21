526b938486098cb0869c577b6b1580fc206a4941
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-21 09:42:52 -0800

    4-print_alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,24 @@
-#inlcude <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * main - prints lowercase alphabet
- * followed by a new line.
+ * followed by a new line,
+ * except q and e.
  * Return: (0) to success
 */
 int main(void)
 {
-  
-  return(0);
+int i;
+
+for (i = 97; i < 123; i++)
+  {
+    if ((i!=101) && (i!=113))
+      {
+      putchar(i);
+      }
+  }
+putchar('\n');
+return (0);
 }
