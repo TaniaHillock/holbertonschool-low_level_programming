6772a48ee426afb8ae4cb5b4543572b4eab2106d
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-21 09:20:36 -0800

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-char l_u_alphabet[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char l_u_alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
 for (i = 0; i < 52; i++)
 {
