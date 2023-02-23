73cdd14e926690c5d6af8566fe4b37305c28478f
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-23 01:36:14 -0800

    line 7

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,15 @@
 */
 void print_alphabet_x10(void)
 {
-int a, b;
+int a;
+int b;
+
 for (a = 0; a <= 9; a++)
-{
-for (b = 97; b <= 122; b++)
-{
-_putchar(b);
-}
-_putchar('\n');
-}
-_putchar('\n');
+ {
+   for (b = 97; b <= 122; b++)
+    {
+      _putchar(b);
+    }
+ _putchar('\n');
+ }
 }
