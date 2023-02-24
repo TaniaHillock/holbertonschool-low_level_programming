c7ddc7a7c5444ce0879039c6efd3c9c1d1141deb
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-24 13:01:16 -0800

    2

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@ int u;
 u = d % 10;
 if (u < 0)
 {
-_putchar(-1 + 48);
-return (-1);
+_putchar(-u + 48);
+return (-u);
 }
 else
 {
-_putchar(1 + 48);
-return (1);
+_putchar(u + 48);
+return (u);
 }
 }
