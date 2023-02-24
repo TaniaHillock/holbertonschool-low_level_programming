4e88e644a49a2fa2ea57f566e39f1c88318beb73
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-24 13:33:29 -0800

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
 *print_to_98 - prints all natural numbers
-*n to 98 followed by space. 
+*n to 98 followed by space.
 *Return: (void) no return.
 */
 void print_to_98(int n)
@@ -11,12 +11,12 @@ if (n > 98)
 {
 for (; n > 98; n--)
 {
-printf("%d, ",n);
+printf("%d, ", n);
 }
 }
 else if (n < 98)
 {
-for (;n < 98; n++)
+for (; n < 98; n++)
 {
 printf("%d, ", n);
 }
