874f93311453def344e05db41c420ecec449452e
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-24 17:23:08 -0800

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*int _isupper(int c); checks for uppercase character
+*Return: (1) is an uppercase character, (0) otherwhise
+*/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);  
+}
+else
+{
+return (0);
+}
+}
