600524901c164d167fc7e8e08ae9dc01c4df1504
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-27 08:35:01 -0800

    0

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/** swap_int swaps the values of two integers
+* @a: the address to first integer
+* @b: the address to second integer
+* Return: Void, no return value
+*/
+void swap_int(int *a, int *b)
+{
+int c = *a
+
+*a = *b;
+*b = c;
+}
