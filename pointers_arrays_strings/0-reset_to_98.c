0818eeb6b89f46844aa5ea566108f15aeae11f99
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-27 08:46:23 -0800

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/** 
+/**
 * reset_to_98 - updates the value it points to 98
 * @n: the address where the value to reset is
 * Return: Void, no return value
--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -6,9 +6,9 @@
 */
 void swap_int(int *a, int *b)
 {
-int c
-*c = *a
+int c;
 
+*c = *a;
 *a = *b;
 *b = *c;
 }
