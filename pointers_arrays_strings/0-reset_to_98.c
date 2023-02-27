3317796aa14fc205b76893f4a795f036036716ac
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-27 08:07:59 -0800

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/** reset_to_98 updates the value it points to 98
+* n@ the address where the value to reset is
+*/Return: Void, no return value
+void reset_to_98(int *n)
+{
+*n = 98;
+}
