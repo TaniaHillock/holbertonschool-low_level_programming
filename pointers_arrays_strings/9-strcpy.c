a2a5424e875815a94941b10aac0dea3127c42297
TaniaHillock <Tania.Hillock@gmail.com>
2023-02-28 10:04:59 -0800

    pointers games

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcpy - copies the string pointed to by src,
+* including the terminating null byte (\0),
+* to the buffer pointed to by dest.
+* @dest: destination.
+* @src: source.
+* Return: the pointer of a char to dest.
+*/
+char *_strcpy(char *dest, char *src)
+{
+int l = 0;
+
+while (l >= 0)
+{
+*(dest + l) = *(src + l);
+if (*(src + l) == '\0')
+break;
+l++;
+}
+return (dest);
+}
