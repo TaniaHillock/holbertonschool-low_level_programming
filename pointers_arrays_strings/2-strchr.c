e81eab2b2b06310abc6f467b16e6b48f60fe54a6
TaniaHillock <Tania.Hillock@gmail.com>
2023-03-02 08:50:08 -0800

    _strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strchr - locates a character in a string.
+* @s : the string evaluated.
+* @c : the first occurence of the character.
+* Return: a pointer to the first c or NULL if not found.
+*/
+
+char *_strchr(char *s, char c)
+{
+int i;
+  
+for( i = 0; *((s + i) != '\0' || (s + i) == 'c'); i++)
+if *(s + i) = '\0'
+{
+return ('\0');
+}
+return (s + i);
+}
