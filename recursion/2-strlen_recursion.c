626e5949b192dbef6a24b3f6221b2ede3a8e7e2f
TaniaHillock <Tania.Hillock@gmail.com>
2023-03-07 02:19:04 -0800

    _strlen_recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#incude "main.h"
+/**
+* _strlen_recursion - returns the lenght of a string.
+* @s : pointer to the string.
+* Return : int @l the lenght of the string.
+*/
+int _strlen_recursion(char *s)
+{
+int l = 0;  
+if (*s != '\0')
+{
+_strlen_recursion(s + 1);
+l++;
+}
+return (l);
+}
