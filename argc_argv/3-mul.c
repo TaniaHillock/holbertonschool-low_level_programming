1397d78cf0b9083ee5b8de88f405a076327fd706
TaniaHillock <Tania.Hillock@gmail.com>
2023-03-07 22:16:19 -0800

    ./mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 */
 int main(int argc, char *argv[])
 {
-int result;  
+int result; 
 if (argc != 3)
 {
 printf("Error\n");
@@ -18,7 +18,7 @@ return (1);
 }
 else
 {
-  result = atoi(argv[1]) * atoi(argv[2]);
+result = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", result);
 return (0);
 }
