c50f84a64892fddb6e5757705015346871a2f854
TaniaHillock <Tania.Hillock@gmail.com>
2023-03-07 22:10:45 -0800

    ./mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,9 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main - prints all arguments it receives.
-* one argument per line, ending with a new line.
+* main - multiplies two numbers.
+* followed by a new line.
 * @argc: the count of arguments.
 * @argv: the array with the strings of arguments.
-* Return: 0 for success.
+* Return: 0 for success, 1 for Error.
 */
+int main(int argc, int *argv[])
+{
+int result;  
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+result = argv[1] * argv[2];
+printf("%d\n", result);
+return (0);
+}
+}
