6a76bbc9a5f6014f37f7f2ad8e7cfaac2157b801
TaniaHillock <Tania.Hillock@gmail.com>
2023-03-07 21:48:05 -0800

    ./args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - prints all arguments it receives.
+* one argument per line, ending with a new line.
+* @argc: the count of arguments.
+* @argv: the array with the strings of arguments.
+* Return: 0 for success.
+*/
+int main(int argc, char *argv[])
+{
+for (int i = 1; i < argc; i++)
+{
+printf("%s\n",argv[i]);
+}
+return (0);
+}
---
+++ argc_argv/4-add.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - prints all arguments it receives.
+* one argument per line, ending with a new line.
+* @argc: the count of arguments.
+* @argv: the array with the strings of arguments.
+* Return: 0 for success.
+*/
