02ce91f72511e6a442cfaa2a6b2114484e1d9336
TaniaHillock <Tania.Hillock@gmail.com>
2023-03-07 21:13:02 -0800

    ./mynameis

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
 * @argv: the array that contains the strings of arguments.
 * Return: 0 if success.
 */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", **argv);
+printf("%s\n", argv[0]);
 return (0);
 }
