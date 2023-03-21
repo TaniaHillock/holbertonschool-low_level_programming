0d95d8d6552e100f375a920676e34b27d17fbc77
taniahillock <tania.hillock@gmail.com>
2023-03-21 17:04:50 -0600

    Pointers to Functions 0 Whats my name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 /**
-* print_name - prints a name.
-* @name: input name.
-* @f: function pointer.
-* Return: no return.
+* print_name - prints a name
+* @name: input name
+* @f: function pointer
+* Return: no return
 */
 void print_name(char *name, void (*f)(char *))
 {
